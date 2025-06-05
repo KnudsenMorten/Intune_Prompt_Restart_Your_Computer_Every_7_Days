25d6974e5187ad9eed25b71e1396173fc89ea355
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2025-06-05 17:09:36 +0200

    Update Restart_Device_Remediation.ps1

--- Restart_Device_Remediation.ps1
+++ Restart_Device_Remediation.ps1
@@ -6,21 +6,9 @@ if (-not (Get-Module -ListAvailable -Name BurntToast)) {
     Import-Module BurntToast -Force
 }
 
-# Threshold for reboot recommendation (set to 1 for testing)
-$ThresholdDays = 7
-
-# Calculate uptime
-$LastBoot = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
-$Days = (New-TimeSpan -Start $LastBoot -End (Get-Date)).Days
-
 # Toast info
 $Title = "Your computer needs to be restarted as it has not been restarted for $Days day(s)."
 $Message = "To ensure the stability and proper functioning of your system, consider rebooting your device very soon."
 
 # Show toast only if uptime exceeds threshold
-if ($Days -ge $ThresholdDays) {
-    if (Test-Path $HeroImage) {
-		# HeroImage + Button (no SnoozeAndDismiss)
-		New-BurntToastNotification -Text $Title, $Message
-	}
-}
+New-BurntToastNotification -Text $Title, $Message
