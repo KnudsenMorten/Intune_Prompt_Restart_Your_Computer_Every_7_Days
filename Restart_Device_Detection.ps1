9ac7a87670c81107f2f8600ecb2c6e96607f083f
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2025-06-05 17:07:40 +0200

    cfg

--- Restart_Device_Detection.ps1
+++ Restart_Device_Detection.ps1
@@ -9,34 +9,33 @@ $Reboot_Delay = 7
 # 									Variables to fill
 ##################################################################################################
 	
-$Last_reboot = Get-ciminstance Win32_OperatingSystem | Select -Exp LastBootUpTime	
+$Last_reboot = Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty LastBootUpTime	
 
 # Check if fast boot is enabled: if enabled uptime may be wrong
 $Check_FastBoot = (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -ea silentlycontinue).HiberbootEnabled 
 
 # If fast boot is not enabled
-If(($Check_FastBoot -eq $null) -or ($Check_FastBoot -eq 0))
+If(( $null -eq $Check_FastBoot ) -or ( 0 -eq $Check_FastBoot ))
 	{
-		$Boot_Event = Get-WinEvent -ProviderName 'Microsoft-Windows-Kernel-Boot'| where {$_.ID -eq 27 -and $_.message -like "*0x0*"}
-		If($Boot_Event -ne $null)
+		$Boot_Event = Get-WinEvent -ProviderName 'Microsoft-Windows-Kernel-Boot' | Where-Object { $_.ID -eq 27 -and $_.message -like "*0x0*" }
+		If($null -ne $Boot_Event)
 			{
 				$Last_boot = $Boot_Event[0].TimeCreated		
 			}
 	}
 ElseIf($Check_FastBoot -eq 1) 	
 	{
-		$Boot_Event = Get-WinEvent -ProviderName 'Microsoft-Windows-Kernel-Boot'| where {$_.ID -eq 27 -and $_.message -like "*0x1*"}
-		If($Boot_Event -ne $null)
+		$Boot_Event = Get-WinEvent -ProviderName 'Microsoft-Windows-Kernel-Boot' | Where-Object { $_.ID -eq 27 -and $_.message -like "*0x1*" }
+		If($null -ne $Boot_Event)
 			{
 				$Last_boot = $Boot_Event[0].TimeCreated		
 			}			
 	}		
 	
-If($Last_boot -eq $null)
+If($null -eq $Last_boot)
 	{
 		# If event log with ID 27 can not be found we checl last reboot time using WMI
 		# It can occurs for instance if event log has been cleaned	
-		$Uptime = $Last_reboot
 	}
 Else
 	{
@@ -56,14 +55,16 @@ $Diff_boot_time = NEW-TIMESPAN –Start $Last_reboot –End $Current_Date
 $Boot_Uptime_Days = $Diff_boot_time.Days	
 $Hour = $Diff_boot_time.Hours
 $Minutes = $Diff_boot_time.Minutes
-$Reboot_Time = "$Boot_Uptime_Days day(s)" + ": $Hour hour(s)" + " : $minutes minute(s)"						
+$Reboot_Time = "$Boot_Uptime_Days day(s)" + " $Hour hour(s)" + " $minutes minute(s)"						
 If($Boot_Uptime_Days -ge $Reboot_Delay)
 	{
+		write-output "RESTART NEEDED !"
 		write-output "Last reboot/shutdown: $Reboot_Time"			
 		EXIT 1		
 	}
 Else
 	{
-		write-output "Last reboot/shutdown: $Reboot_Time"			
+		write-output "NO RESTART NEEDED"
+		write-output "Last reboot/shutdown: $Reboot_Time"
 		EXIT 0
 	}		
